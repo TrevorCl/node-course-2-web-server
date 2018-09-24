eea6897245a3f7d0574710dc3b24fcbd53862aac
Trevor Clulow <trevorclulow@Trevors-MacBook-Pro.local>
2018-09-24 14:55:42 +0100

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const hbs = require('hbs');
 const fs=require('fs');
 
+var port=process.env.PORT || 3000;
+
 var app=express();
 
 // 1. retrieve static pages
@@ -19,12 +21,12 @@ app.use((req, res, next)=> {
    next();
 });
 
-app.use( (req,res, next)=> {
-   res.render('maintenance.hbs',{
-     pageTitle:'Maintenance',
-     currentYear:2017
-   });
-})
+// app.use( (req,res, next)=> {
+//    res.render('maintenance.hbs',{
+//      pageTitle:'Maintenance',
+//      currentYear:2017
+//    });
+// })
 
 
 hbs.registerHelper('getCurrentYear',()=> {
@@ -75,6 +77,6 @@ app.get('/bad', (req,res) => {
 
 
 // start server listening
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
